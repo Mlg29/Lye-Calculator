7def4d3299ea4ce288b99f24a71f1518f9685a29
SALIMONU AYODELE KABIR <39867606+ayodelesalimon@users.noreply.github.com>
2020-02-18 09:28:10 +0100

    Print result component

--- src/component/Result/index.jsx
+++ src/component/Result/index.jsx
@@ -1,8 +1,43 @@
 import React from "react"
 import "./Result.css"
+import ReactToPrint from 'react-to-print';
 
+
+ 
 
 class Result extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            
+        }
+    }
+   
+    render() {
+        
+        return (
+           
+        <div>
+        <ReactToPrint
+          trigger={() => <a href="#">Print this out!</a>}
+          content={() => this.componentRef}
+        />
+        <ComponentToPrint ref={el => (this.componentRef = el)}
+           
+        />
+      </div>
+            
+
+        )
+       
+      
+    }
+    
+}
+
+export default Result
+
+class ComponentToPrint extends React.Component {
     constructor() {
         super()
         this.state = {
@@ -27,56 +62,54 @@ class Result extends React.Component {
 
         function fragnance() {
                 switch(fragrance) {
-                case 'None':
-                    return Math.floor(totalOil * 0)
+                case "None":
+                    return Math.floor(totalOil/1)
                 case "Light":
-                    return Math.floor(totalOil * ( 2 / 100) )
+                    return Math.floor(totalOil / 2)
                 case "Medium":
-                    return Math.floor(totalOil * ( 3.5 / 100) )
+                    return Math.floor(totalOil / 3.5)
                 case "Strong":
-                    return Math.floor(totalOil * ( 5 / 100) )
+                    return Math.floor(totalOil / 5 )
                 default:
                     break
 
             }
         }
-        
-        return (
-            <div className="result">
-                    <header className="result-header">Result</header>
-                    <h5 className="result-header2"> A <b>{type}</b> soap, measured in <b>{measure}</b> with superfat of <b>{level}%</b> using a <b>{fragrance}</b> Fragrance strength</h5>
-                 <p className="result-paragraph">Total Amount of Lye Needed: <strong>
-                     {
-                        type === "Liquid" ? Math.floor(totalAmountOfLyeLiquid) : Math.floor(totalAmountOfLyeSolid)
-                     }g
-                     </strong>
-                </p>
-                <p className="result-paragraph2">Total Amount of Water Needed:<strong>
-                    {
-                        type === "Liquid" ? Math.floor(totalAmountOfWaterLiquid) : Math.floor(totalAmountOfWaterSolid) 
-                    }g
-                    </strong>                                                                               
-                </p>
-                <p className="result-paragraph3">Superfatting Discount: <strong>
-                    {
-                        type === "Liquid" ? Math.floor(superfattingValueLiquid) : Math.floor(superfattingValueSolid) 
-                    }%
-                    </strong>                  
-                </p>
-                <p className="result-paragraph2">Amount of Fragrance: <strong>
-                    {
-                      fragnance()
-                    }g
-                    </strong>                  
-                </p>
-                <p className="result-paragraph3">Total Batch Size: <strong>
-                    {totalOil}g
-                    </strong>                  
-                </p>
-            </div>
-        )
-    }
+  
+      return (
+        <div className="result">
+        <header className="result-header">Result</header>
+        <h5 className="result-header2"> A <b>{type}</b> soap, measured in <b>{measure}</b> with superfat of <b>{level}%</b> using a <b>{fragrance}</b> Fragrance strength</h5>
+     <p className="result-paragraph">Total Amount of Lye Needed: <strong>
+         {
+            type === "Liquid" ? Math.floor(totalAmountOfLyeLiquid) : Math.floor(totalAmountOfLyeSolid)
+         }g
+         </strong>
+    </p>
+    <p className="result-paragraph2">Total Amount of Water Needed:<strong>
+        {
+            type === "Liquid" ? Math.floor(totalAmountOfWaterLiquid) : Math.floor(totalAmountOfWaterSolid) 
+        }g
+        </strong>                                                                               
+    </p>
+    <p className="result-paragraph3">Superfatting Discount: <strong>
+        {
+            type === "Liquid" ? Math.floor(superfattingValueLiquid) : Math.floor(superfattingValueSolid) 
+        }g
+        </strong>                  
+    </p>
+    <p className="result-paragraph2">Amount of Fragrance: <strong>
+        {
+          fragnance()
+        }g
+        </strong>                  
+    </p>
+    <p className="result-paragraph3">Total Oil: <strong>
+        {totalOil}g
+        </strong>                  
+    </p>
     
+</div>
+      );
+    }
 }
-
-export default Result
